a839556b3c52974ec8197433263407ae1638af9e
stabl <stabl.j9@gmail.com>
2025-10-14 16:22:16 +0200

    new configs

--- i3/wrapper.sh
+++ i3/wrapper.sh
@@ -2,6 +2,6 @@
 
 # Run i3status and prepend custom brightness info
 i3status | while read -r line; do
-    myoutput="$("$HOME/.config/i3status/brightness.sh")"
+    myoutput="$("$HOME/.config/i3/brightness.sh")"
     echo "$myoutput | $line" || exit 1
 done
