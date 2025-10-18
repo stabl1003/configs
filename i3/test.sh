017213b90cd59f20c4e00a01285037b3cb1c0f7e
stabl <stabl.j9@gmail.com>
2025-10-18 19:23:17 +0200

    layout in i3status

--- i3/test.sh
+++ i3/test.sh
@@ -3,5 +3,5 @@
 i3status | while read -r line; do
     brightness="$("$HOME/.config/i3/brightness.sh")"
     layout=$(setxkbmap -query | awk '/layout/{print $2}')
-     echo "$brightness | $layout | $line" || exit 1
- done
+    echo "$brightness | $layout | $line" || exit 2
+done
